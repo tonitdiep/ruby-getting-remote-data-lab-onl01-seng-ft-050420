7bc9cf8ae3d04dceb13c2af54c01c7af90e09c30
Toni  Diep <adiep123@gmail.com>
2020-05-23 06:14:53 +0000

    Done.

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,7 @@
 # Write your code here
+class GetRequester
+  url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+#string url
+#get_response_body method that sends a GET request to the URL passed in on intialization return body of the response
+#parse_json method using get_response_body to send a request, return a Ruby Array/Hash
+end
